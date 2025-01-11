6c2ac3bf1c376c858746f0e0f3f301da2a228637
Gabriel Evaristo <gabriel.evaristo.2004@gmail.com>
2025-01-10 22:57:45 -0300

    feat: initial commit

---
+++ src/main/java/io/github/gabrielhenrique/minha_agenda_api/MinhaAgendaApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.gabrielhenrique.minha_agenda_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaAgendaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaAgendaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gabrielhenrique/minha_agenda_api/MinhaAgendaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabrielhenrique.minha_agenda_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaAgendaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
